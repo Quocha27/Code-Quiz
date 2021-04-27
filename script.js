0d48fa1b935468a32de6c78bebb35a57e122cfc7
Quocha27 <80284662+Quocha27@users.noreply.github.com>
2021-04-27 11:03:28 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-var startQuiz = document.querySelector("#startBtn");
-var timeTracker = document.querySelector("#timeR");
-var instructions = document.querySelector("Instructions");
-var questionDisplay = document.querySelector("#question");
-var showResults = document.querySelector("#results");
